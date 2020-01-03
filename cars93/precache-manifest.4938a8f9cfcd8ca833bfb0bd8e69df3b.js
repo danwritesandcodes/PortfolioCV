66fc8641cf96af3d80e7b4c6260dd25065fcd26c
danwritesandcodes <danwritesandcodes@gmail.com>
2020-01-03 07:16:52 +0000

    Added cars93 compiled React App

---
+++ cars93/precache-manifest.4938a8f9cfcd8ca833bfb0bd8e69df3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a462be25991ea153d2be5d302834d2af",
+    "url": "/cars93/index.html"
+  },
+  {
+    "revision": "f588c292f435733b9c71",
+    "url": "/cars93/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9598dc1179d2d37a7454",
+    "url": "/cars93/static/js/2.dae986f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cars93/static/js/2.dae986f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f588c292f435733b9c71",
+    "url": "/cars93/static/js/main.a27978c5.chunk.js"
+  },
+  {
+    "revision": "658b1d222f740bcae20a",
+    "url": "/cars93/static/js/runtime-main.30abf680.js"
+  }
+]);
